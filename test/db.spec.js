ffe3fe779c8f3d85592586e033344b8e50ce924c
Vladimir Bukhin <vlad@jut.io>
2016-02-26 10:47:36 -0800

    ensure global db client isn't effected by dynamic conf changes.

--- test/db.spec.js
+++ test/db.spec.js
@@ -1,5 +1,6 @@
-require('./shared');
+var config = require('./shared');
 
+var _ = require('underscore');
 var expect = require('chai').expect;
 var TestUtils = require("./utils");
 var check_juttle = TestUtils.check_juttle;
@@ -11,6 +12,21 @@ describe('test db connection error', function () {
         })
         .then(function(result) {
             expect(result.errors[0]).to.contain('connect to database');
+        });
+    });
+
+    it('2 reads with different dbs confs', function() {
+        return check_juttle({
+            program: 'read opentsdb -from :-30m: -to :now: -name "' + TestUtils.metric_name + '" | view json;' +
+                'read opentsdb -id "fake" -from :30 minutes ago: -name "' + TestUtils.metric_name + '";',
+            realtime: true
+        }, 3000)
+        .then(function(result) {
+            expect(result.errors).to.have.length(1);
+            expect(result.errors[0]).to.contain('connect to database');
+
+            var fakeConfig = _.findWhere(config, {id: 'fake'});
+            expect(result.errors[0]).to.contain(fakeConfig.host);
         });
     });
 });
